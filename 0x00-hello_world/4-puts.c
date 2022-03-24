54607e257f543db134e73b76fa4bd6751493ad79
Ekarisa <ekarisa1992@gmail.com>
2022-03-24 04:48:40 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * main - prints "programming is like building a multiligual puzzle"
+ * Return : Always 0.
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
