426f98c82b60be566bebf49ea1dafcc4ab067262
Ekarisa <ekarisa1992@gmail.com>
2022-03-25 07:54:12 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 /**
- * main - prints sizes of differnt types
+ * main - Entry point sizes of differnt types
+ * cisfun
  * Return: always 0
  */
 int main(void)
 {
 
 		
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of long int: %d byte(s)\n", sizeof(long int));
-	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-	prints("size of float: %d byte(s)\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %zu byte(s)\n", sizeof(long long int));
+	prints("size of float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 
