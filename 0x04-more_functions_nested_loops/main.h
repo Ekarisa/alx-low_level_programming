ade8d9b3d6434cada6ea300cb141ab20dd053d14
Your Name <ekarisa1992@gmail.com>
2022-06-11 10:34:57 -0700

    functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void fizz_buzz(void);
-void print_triangle(int size);
+void print_triangle(int size)i;
+void print_number(int n);
